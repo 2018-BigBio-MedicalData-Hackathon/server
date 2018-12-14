52def08ed65ab07cc4ab261ea0d2d42ee0fe00a7
schio <ghsehr1@gmail.com>
2018-12-15 08:21:17 +0900

    add getMedicineImg

---
+++ getMedicineImg.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from pprint import pprint as p
+import re
+from datetime import datetime
+
+def getDriver(url):
+    options = Options()
+    #head less mode
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+
+    #for head less mode detection
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36") # agent change 
+    options.add_argument("lang=ko_KR") 
+
+    # driver setting
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r'/home/ubuntu/install/chromedriver')
+    driver.get(url)
+
+    # for head less mode detection
+    driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});") #num of plugin spoofing
+    driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
+    driver.execute_script("const getParameter = WebGLRenderingContext.getParameter;WebGLRenderingContext.prototype.getParameter = function(parameter) {if (parameter === 37445) {return 'NVIDIA Corporation'} if (parameter === 37446) {return 'NVIDIA GeForce GTX 980 Ti OpenGL Engine';}return getParameter(parameter);};")
+
+    return driver
+
+def getImg(medicineName):
+
+    # 약학정보원
+    driver = getDriver('http://www.health.kr/searchDrug/search_detail.asp')
+
+    # 약품명 입력
+    driver.find_element_by_name('input_drug_nm').send_keys(medicineName)
+
+    # 검색버튼 클릭
+    driver.find_element_by_xpath('//*[@id="btn_detail_search"]').click()
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # data=[]
+    data=soup.select('#tbl_proY > tbody > tr > td > img')#tbl_proY > tbody > tr:nth-child(2)
+    
+    for img in data:
+        return img.get("src")
+
+if __name__ == '__main__':
+    medicines=['프레드포르테점안액', '오큐시클로점안액', '솔로젠정', '록소젠정', '파모시드정20mg']
+    for name in medicines:
+        print(name, getImg(name))
+    
