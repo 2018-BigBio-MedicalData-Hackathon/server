211534aba2e46cecf4189facfba8cf979e39d015
Jeon Ga Bin <hyanghope@naver.com>
2018-12-15 17:49:20 +0900

    login 구현 완료

--- person.py
+++ person.py
@@ -1,6 +1,5 @@
 
 
-
 "병원 등에게서 처방전, 영수증을 받기 위한 API"
 
 from flask import Flask, request
--- signup.py
+++ signup.py
@@ -40,30 +40,38 @@ class Login(Resource):
             _parser = reqparse.RequestParser()
             _parser.add_argument('userid', type=str)
             _parser.add_argument('password', type=str)
+            _parser.add_argument('username', type=str)
+            _parser.add_argument('reginum', type=int)
+            _parser.add_argument('phonenum', type=int)
+            _parser.add_argument('email', type=str)
             _args = _parser.parse_args()
 
             # 변수에 할당
             _userid = conn.escape_string(_args['userid'])
             _password = conn.escape_string(_args['password'])
+            _username = conn.escape_string(_args['username'])
+            _reginum = conn.escape_string(_args['reginum'])
+            _phonenum = conn.escape_string(_args['phonenum'])
+            _email = conn.escape_string(_args['email'])
 
             # salt 데이터베이스에서 가져오기
-            _query = "select salt from user where userid='%s'" % (_userid)
+            _query = "select salt from user where username='%s'" % (_username)
             cursor.execute(_query)
             _data = cursor.fetchone()
             _salt = _data[0]
 
             # 회원 id verify
             _password = makepasswd(_password, _salt)
-            print(_userid, _password)
-            _query = "select userid from user where passwd='%s'" % (_password)
+            print(_username, _password)
+            _query = "select username from user where passwd='%s'" % (_password)
             cursor.execute(_query)
             _data = cursor.fetchone()
             print(_data[0])
-            if _data[0] != _userid:
+            if _data[0] != _username:
                 return {"id and password are wrong": 401}
 
             # 회원 password verify 
-            _query = "select password from user where id='%s'" % (_userid)
+            _query = "select password from user where id='%s'" % (_username)
             cursor.execute(_query)
             newpassword = cursor.fetchone()[0]
             if(_password == newpassword):
